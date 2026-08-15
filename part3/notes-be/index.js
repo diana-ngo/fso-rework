ab404c8a85282976c4fe7d79654ac6af61c3da2e
Diana Ngo <dngo199@gmail.com>
2026-08-15 17:57:21 -0400

    pushing whole app

--- part3/notes-be/index.js
+++ part3/notes-be/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-app.use(cors())
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method: ', request.method)
