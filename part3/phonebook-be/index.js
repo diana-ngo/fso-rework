60b2faf9a7e5e4649c6051e56170ea7c13a05f00
Diana Ngo <dngo199@gmail.com>
2026-08-15 21:31:43 -0400

    uibuild

--- part3/phonebook-be/index.js
+++ part3/phonebook-be/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.use(morgan((tokens, req, res) => {
   return [
     tokens.method(req, res),
@@ -96,8 +97,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
