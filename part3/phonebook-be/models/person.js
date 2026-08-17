5e1638aef39f03c73e4b292ad9b3c704a931a5e9
Diana Ngo <dngo199@gmail.com>
2026-08-17 17:54:25 -0400

    uibuild

--- part3/phonebook-be/models/person.js
+++ part3/phonebook-be/models/person.js
@@ -10,8 +10,21 @@ mongoose
   .catch((error) => console.log(`Unable to connect to MongoDB: ${error}`))
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'Phone number required']
+  }
 })
 
 personSchema.set('toJSON', {
